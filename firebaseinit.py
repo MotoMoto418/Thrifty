cdf20ed790c555a907cca1ba5f609f624d6f35f0
MotoMoto <66240313+MotoMoto418@users.noreply.github.com>
2022-05-20 17:36:12 +0530

    Update firebaseinit.py

--- firebaseinit.py
+++ firebaseinit.py
@@ -3,10 +3,10 @@ class Firebase:
         import pyrebase
 
         firebaseConfig = {
-            'apiKey': "AIzaSyCaQWEJH7f3rhY5WTwACzBPrZI3MQqr2fs",
+            'apiKey': "NO",
             'authDomain': "price-tracker-b10f5.firebaseapp.com",
-            'databaseURL': "https://price-tracker-b10f5-default-rtdb.asia-southeast1.firebasedatabase.app",
-            'projectId': "price-tracker-b10f5",
+            'databaseURL': "NO",
+            'projectId': "NO",
             'storageBucket': "price-tracker-b10f5.appspot.com",
             'messagingSenderId': "334244448099",
             'appId': "1:334244448099:web:3968d02d8e6ec6ea1e3556",
